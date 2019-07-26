2c0e249ca930c43a55014cea0e7b5b75cd84e00c
Veluswamy <kveluswamy@kv-4usjhd2-mbp.bamtech.dss.media>
2019-07-26 16:55:18 -0400

    Scala Test

---
+++ scala/Bar.scala
@@ -0,0 +1,3 @@
+object Bar {
+  def hello() = "Hello"
+}
---
+++ scala/Main.scala
@@ -0,0 +1,10 @@
+object Main extends App{
+
+  //println("ABCD")
+  //def main(args: Array[String]) = {
+    println(Bar.hello())git status
+    val f = new Foo()
+    f.foo()
+  //}
+}
+
