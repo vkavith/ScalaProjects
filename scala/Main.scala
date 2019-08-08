a847a6d0fce65d85ca58ecedcdd43550e369f2ac
Veluswamy <kveluswamy@kv-4usjhd2-mbp.bamtech.dss.media>
2019-08-08 16:57:11 -0400

    Sample Scala Hello World

--- scala/Main.scala
+++ scala/Main.scala
@@ -2,7 +2,7 @@ object Main extends App{
 
   //println("ABCD")
   //def main(args: Array[String]) = {
-    println(Bar.hello())git status
+    println(Bar.hello())
     val f = new Foo()
     f.foo()
   //}
